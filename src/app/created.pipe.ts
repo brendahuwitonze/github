77d9e6a3ffc357c4e47720952e1c04d99dfca6a4
brendahuwitonze <uwitonzebrendah@gmail.com>
2020-11-10 08:32:38 +0200

    added values

--- src/app/created.pipe.ts
+++ src/app/created.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CreatedPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): any{
+    let currentYear: any = new Date().getFullYear(); //2020
+    let githubCreatedYear:any = new Date(value).getFullYear();
+    let githubYear = currentYear - githubCreatedYear;
+
+    return githubYear;
   }
 
 }
